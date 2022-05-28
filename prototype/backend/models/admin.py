57e8e8a367d6bdc9f0424fe861f2a4414b995a7a
Merlin-dot <81772179+Merlin-dot@users.noreply.github.com>
2022-05-28 16:52:49 +0200

    Added syntax for the required tables

--- prototype/backend/models/admin.py
+++ prototype/backend/models/admin.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Admin(db.Model):
-    __tablename__ = "adminen"
+    __tablename__ = "admin"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
---
+++ prototype/backend/models/organisation.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Organisation(db.Model):
+    __tablename__ = "organisation"
+
+    orgaid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(6900000), unique=False, nullable=False)
+    permission = db.Column(db.String(4200000), unique=False, nullable=False)
+
+    def __init__(self, name, permission):
+        self.name = name
+        self.permission = permission
