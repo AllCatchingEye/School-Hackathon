a5a3261b544336bf59f3b69a3b729ab4c53032fb
georgLang9 <langgeorg61@gmail.com>
2022-05-29 14:32:15 +0200

    Changed design and added post to form

---
+++ prototype/frontend/src/components/UserLogin.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="control">
+  <form>
+    <label for="username" class="label">Benutzername: </label>
+    <input type="text" id="username" class="input is-large" placeholder="Benutzername..." required><br><br>
+    
+    <label for="password" class="label">Passwort: </label>
+    <input type="password" id="password" class="input is-large" placeholder="Passwort..." required><br><br>
+
+    <button class="button is-primary" type="submit">Anmelden</button>
+  </form>
+</div>
+
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: 'UserLogin',
+    data() {
+  },
+  methods: {
+    }
+};
+</script>
+
+<style>
+</style>
