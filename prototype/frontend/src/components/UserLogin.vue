461db82cd2b9649f44ece85ac196d697635871fb
georgLang9 <langgeorg61@gmail.com>
2022-06-01 17:43:56 +0200

    Changed background

--- prototype/frontend/src/components/UserLogin.vue
+++ prototype/frontend/src/components/UserLogin.vue
@@ -1,16 +1,46 @@
 <template>
-<div class="control">
-  <form>
-    <label for="username" class="label">Benutzername: </label>
-    <input type="text" id="username" class="input is-large" placeholder="Benutzername..." required><br><br>
-    
-    <label for="password" class="label">Passwort: </label>
-    <input type="password" id="password" class="input is-large" placeholder="Passwort..." required><br><br>
-
-    <button class="button is-primary" type="submit">Anmelden</button>
-  </form>
-</div>
-
+  <section class="hero is-primary is-fullheight" id="login-form">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+            <img src="../assets/wirfuerschuleLogo.png" alt="wirfuerschule logo"/>
+            <form action="" class="box">
+              <div class="field">
+                <label for="" class="label">Email</label>
+                <div class="control has-icons-left">
+                  <input type="email" placeholder="e.g. bobsmith@gmail.com" class="input" required>
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-envelope"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="field">
+                <label for="" class="label">Password</label>
+                <div class="control has-icons-left">
+                  <input type="password" placeholder="*******" class="input" required>
+                  <span class="icon is-small is-left">
+                    <i class="fa fa-lock"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="field">
+                <label for="" class="checkbox">
+                  <input type="checkbox">
+                Remember me
+                </label>
+              </div>
+              <div class="field">
+                <button class="button is-success">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -25,4 +55,7 @@ export default {
 </script>
 
 <style>
+#login-form {
+  background-color: whitesmoke;
+}
 </style>
