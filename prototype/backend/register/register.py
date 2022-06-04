dd73152168faa26f27e94e160ce99cc5d9f90489
merlin-dot <merlinreiter@live.com>
2022-06-04 13:07:34 +0000

    Added register api for adding user

--- prototype/backend/register/register.py
+++ prototype/backend/register/register.py
@@ -5,6 +5,8 @@ from sqlalchemy.exc import IntegrityError
 from flask_expects_json import expects_json
 from models.organisation import Organisation
 from models.roles import Roles
+from utils.user_roles import auth_required, Config
+from flask_jwt_extended import create_access_token, get_jwt
 
 Register = Blueprint('register', __name__)
 register_schema = {
@@ -21,48 +23,40 @@ register_schema = {
 }
 
 
-@Register.route('/teacher/', methods=['POST'])
+@Register.route('/', methods=['POST'])
+@auth_required([Config.ADMIN_ID, Config.SUPERADMIN_ID])
 @expects_json(register_schema) # Compares request schema with expected schema 
-def create_teacher():
+def create():
     """
         Creates a teacher.
     """
-    user_data = request.get_json()
-    print(user_data)
-    user = User(**user_data)
-    db.session.add(user)
-    try:
-        db.session.commit()
-        result = jsonify(
-                category="Success", 
-                message=f"User: {user.name} with E-Mail: {user.email} created"), 201
-    except IntegrityError as e:
-        db.session.rollback()
-        result = jsonify(
-                category="Error", 
-                message=f"{e}"), 409
-    
+    valid = False
+    result = jsonify(category = "Error", message="You are not allowed to send this request")
+
+    data_request = request.get_json()
+    if get_jwt()["role"] == Config.ADMIN_ID:
+        if data_request["role"] in [Config.ADMIN_ID, Config.TEACHER]:
+            valid = True
+    elif get_jwt()["role"] == Config.SUPERADMIN_ID:
+        if data_request["role"] in [Config.ADMIN_ID, Config.SUPERADMIN_ID]:
+            valid = True
+    if valid:
+        user_data = request.get_json()
+        print(user_data)
+        user = User(**user_data)
+        db.session.add(user)
+        try:
+            db.session.commit()
+            result = jsonify(
+                    category="Success",
+                    message=f"User: {user.name} with E-Mail: {user.email} created")#, 201
+        except IntegrityError as e:
+            db.session.rollback()
+            result = jsonify(
+                    category="Error",
+                    message=f"{e}")#, 409
+
     return result
-
-
-@Register.route('/superadmin/', methods=['POST'])
-@expects_json(register_schema) # Compares request schema with expected schema 
-def create_super_admin():
-    """
-        Creates a super admin.
-    """
-    pass
-    
-
-
-@Register.route('/admin/', methods=['POST'])
-@expects_json(register_schema) # Compares request schema with expected schema 
-def create_admin():
-    """
-        Creates an admin.
-    """
-    pass
-
 
 # Testing method to add something to database
 @Register.route('/add/', methods=['GET'])
@@ -70,7 +64,23 @@ def add_to_db():
     """
         Add to DB
     """
-    role = Roles(420, "Superadmin", "Total Access")
+    """    role = Roles(420, "Superadmin", "Total Access")
+    role = Roles(29, "Admin", "Normal Access")
     db.session.add(role)
     db.session.commit()
-    return str(role)
+    role = Roles(12, "Teacher", "Teacher Access")
+    db.session.add(role)
+    db.session.commit()
+    role = Roles(1, "User", "User Access")
+    db.session.add(role)
+    db.session.commit()
+
+    organisation = Organisation("MUCDAI")
+    db.session.add(organisation)
+    db.session.commit()"""
+
+    organisation = User("muc@dealer.com", "Mucdai","Mucdai", "muc", 420, 1)
+    db.session.add(organisation)
+    db.session.commit()
+
+    return str(organisation)
