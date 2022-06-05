b27c5a404d1be1cbfb550a27f490be27535314c1
merlin-dot <merlinreiter@live.com>
2022-06-05 15:46:58 +0000

    fixed error in logout caused by changes

--- prototype/backend/logout/logout.py
+++ prototype/backend/logout/logout.py
@@ -6,7 +6,7 @@ from utils.user_roles import auth_required, Config
 Logout = Blueprint('logout', __name__)
 
 @Logout.route("/", methods=["POST"])
-@auth_required([Config.ADMIN_ID, Config.SUPERADMIN_ID, Config.TEACHER])
+@auth_required([Config.ADMIN_ID, Config.SUPERADMIN_ID, Config.TEACHER_ID])
 def logout():
     response = jsonify(
         category="Success",
