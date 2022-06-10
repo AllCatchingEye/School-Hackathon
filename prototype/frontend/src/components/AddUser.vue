fedbfbf6b56b210b3136ef5ffc7da5cdca937015
mthoma <mthoma@hm.edu>
2022-06-10 20:29:43 +0200

    changed organisation input from numbers to dropdown menu

--- prototype/frontend/src/components/AddUser.vue
+++ prototype/frontend/src/components/AddUser.vue
@@ -38,36 +38,47 @@
     <div v-if="isLoadedRole === true">
         <div class="field">
             <label class="label">Rolle</label>
-                <div class="dropdown is-hoverable">
-                    <div class="dropdown-trigger">
-                        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                            <span> {{ choosenRole }} </span>
-                            <span class="icon is-small">
-                                <i class="fas fa-angle-down" aria-hidden="true"></i>
-                            </span>
-                        </button>
-                    </div>
-                    <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                        <div class="dropdown-content">
-                            <a v-for="rolle in roles" :key="rolle.roleid">
-                                <a href="#" class="dropdown-item" @click="changeRole(rolle.description, rolle.roleid)">
-                                    {{ rolle.description }}
-                                </a>
+            <div class="dropdown is-hoverable">
+                <div class="dropdown-trigger">
+                    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                        <span> {{ choosenRole }} </span>
+                        <span class="icon is-small">
+                            <i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </span>
+                    </button>
+                </div>
+                <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                    <div class="dropdown-content">
+                        <a v-for="rolle in roles" :key="rolle.roleid">
+                            <a href="#" class="dropdown-item" @click="changeRole(rolle.description, rolle.roleid)">
+                                {{ rolle.description }}
                             </a>
-                        </div>
+                        </a>
                     </div>
                 </div>
+            </div>
         </div>
     </div>
-    <div v-if="userRole == 420 && isLoadedRole === true">
+    <div v-if="userRole == 420 && isLoadedOrga === true">
         <div class="field">
-            <div class="control has-icons-left has-icons-right">
-                <label class="label">Organisation</label>
-                <div class="control">
-                    <input v-model="organisation" class="input is-medium" type="number" placeholder="e.g. 1" required>
-                    <span class="icon is-left">
-                        <i class="fas fa-solid fa-house"></i>
-                    </span>
+            <label class="label">Organisation</label>
+            <div class="dropdown is-hoverable">
+                <div class="dropdown-trigger">
+                    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                        <span> {{ choosenOrga }} </span>
+                        <span class="icon is-small">
+                            <i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </span>
+                    </button>
+                </div>
+                <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                    <div class="dropdown-content">
+                        <a v-for="organisation in orgas" :key="organisation.orgaid">
+                            <a href="#" class="dropdown-item" @click="changeOrga(organisation.name, organisation.orgaid)">
+                                {{ organisation.name }}
+                            </a>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -75,7 +86,6 @@
     <div>
         <button class="button is-success is-rounded" @click="addNewUser">Submit</button>
     </div>
-    {{ organisation }}
 </template>
 
 <script>
@@ -90,6 +100,7 @@ export default {
     data() {
         return {
             isLoadedRole: false,
+            isLoadedOrga: false,
             choosenRole: 'none',
             role: 0,
             name: '',
@@ -99,17 +110,24 @@ export default {
             userRole: 0,
             error: false,
             message: '',
+            choosenOrga: 'none',
+            orgas: {},
         }
     },
     mounted() {
         this.getRoleInfo();
         this.getCurrentRole();
         this.getOrgaID();
+        this.getOrgaInfo();
     },
     methods: {
         changeRole(text, id) {
             this.choosenRole = text;
             this.role = id;
+        },
+        changeOrga(text, id) {
+            this.choosenOrga = text;
+            this.organisation = id;
         },
         getRoleInfo() {
             const path = '/api/role/'
@@ -145,6 +163,20 @@ export default {
             })
                 .then((response) => {
                     this.organisation = response.orgaid;
+                })
+                .catch((err) => {
+                    console.log(err);
+                    console.log(this.cookies)
+                })
+        },
+        getOrgaInfo() {
+            const path = '/api/organisation/'
+            axios.get(path, {
+                withCredentials: true
+            })
+                .then((response) => {
+                    this.isLoadedOrga = true;
+                    this.orgas = response.data;
                 })
                 .catch((err) => {
                     console.log(err);
