fbba8c7c3ea4bbae98869b02ed00384c0c604e1c
Lionel Wisskirchen <lionel.wisskirchen@hm.edu>
2022-06-12 16:56:19 +0200

    Added query to restrict token generation for other organisation hackathons

--- prototype/backend/api/token/token.py
+++ prototype/backend/api/token/token.py
@@ -1,6 +1,8 @@
+from sys import orig_argv
 from flask import Blueprint, jsonify, request
 from models.token import Token as Tokenmodel
 from models.user import User as Usermodel
+from models.hackathon import Hackathon as Hackathonmodel
 from utils.user_roles import auth_required, Config
 from flask_jwt_extended import get_jwt_identity, get_jwt
 from app import db
@@ -9,22 +11,30 @@ Token = Blueprint('token', __name__)
 @Token.route('/', methods=['POST'])
 @auth_required([Config.ADMIN_ID, Config.TEACHER_ID])
 def generate_token():
-    hackathonid=request.get_json()["hackathon"]
+    hackathon_id=request.get_json()["hackathon"]
     number_of_token=request.get_json()["count"]
     assert number_of_token > 0
-    user = Usermodel.query.filter_by(email=get_jwt_identity()).first()
-    for x in range(0,number_of_token):
-        token = Tokenmodel(user.userid, hackathonid)
-        db.session.add(token)            
+    organisation  = get_jwt()["organisation"]
+    is_valid = Hackathonmodel.query.filter_by(organisationid = organisation, hackathonid = hackathon_id).first()
+
+    if is_valid: 
+        user = Usermodel.query.filter_by(email=get_jwt_identity()).first()
+        for x in range(0,number_of_token):
+            token = Tokenmodel(user.userid, hackathon_id)
+            db.session.add(token)            
+        
+        try:
+            db.session.commit() 
+        except:
+            db.session.rollback()
     
-    try:
-        db.session.commit() 
-    except:
-        db.session.rollback()
-    
-    return (jsonify(
+        return (jsonify(
                     category="Success",
                     message=f"Created {number_of_token} Token"), 201)
+    else:
+        return (jsonify(
+                    category="Error",
+                    message=f"Not allowed"), 401)
 
 
 @Token.route('/', methods=['GET'])
