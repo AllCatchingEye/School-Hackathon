2a6a80a8ae6b65609f3644bfc2160be08f24d694
Lionel Wisskirchen <lionel.wisskirchen@hm.edu>
2022-06-19 09:54:13 +0200

    changed token endpoint to return added tokens

--- prototype/backend/models/token.py
+++ prototype/backend/models/token.py
@@ -5,11 +5,12 @@ from sqlalchemy_serializer import SerializerMixin
 class Token(db.Model, SerializerMixin):
     __tablename__ = "token"
 
-    tokenid = db.Column(db.String,  default=lambda: str(uuid.uuid4()), primary_key=True)
+    tokenid = db.Column(db.String, primary_key=True)
     hackathonid = db.Column(db.Integer, db.ForeignKey('hackathon.hackathonid'))
     hackathon = db.relationship('Hackathon')
     userid = db.Column(db.Integer, db.ForeignKey('users.userid'))
 
     def __init__(self, userid, hackathonid):
         self.hackathonid = hackathonid
+        self.tokenid =  str(uuid.uuid4())
         self.userid = userid
