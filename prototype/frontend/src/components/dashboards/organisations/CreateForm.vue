8ee47fdd353bca5fa6064fabdbddee5c3c59f6e6
georgLang9 <langgeorg61@gmail.com>
2022-06-19 10:08:29 +0200

    copied new design files to organisation

---
+++ prototype/frontend/src/components/dashboards/organisations/CreateForm.vue
@@ -0,0 +1,110 @@
+<template>
+
+<div class="modal delete-model">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+        <header class="modal-card-head">
+        <p class="modal-card-title">Add User</p>
+        <button class="delete" aria-label="close" @click="cancel"></button>
+        </header>
+        <section class="modal-card-body">
+            <form class="add-item">
+                <label for="name">Name</label>
+                <input type="text" id="name" required v-model="Data.name" />
+                
+                <label for="firstname">Vorname</label>
+                <input type="text" id="firstname" required v-model="Data.firstname" />
+                
+                <label for="email">E-mail</label>
+                <input  type="email" id="email" required v-model="Data.email" />
+
+                <label v-if="Organisations.length"  for="organisation">Organisation</label>
+                <select v-if="Organisations.length" class="select" required v-model="Data.organisation">
+                    <option selected disabled value="0">Choose Organisation</option>
+                    <option v-for="organisation in Organisations" 
+                            :value="organisation.orgaid" 
+                            :key="organisation.orgaid">{{organisation.name}}</option>
+                </select>  
+
+                <label for="role">Rolle</label>
+                    <select class="select" required v-model="Data.role">
+                    <option selected disabled value="0">Choose Role</option>
+                    <option v-for="role in Roles" 
+                            :value="role.roleid" 
+                            :key="role.roleid">{{role.description}}</option>
+                </select>       
+            </form>
+        </section>
+        <footer class="modal-card-foot">
+            <button class="button is-danger is-rounded" @click="cancel">Cancel</button>
+            <button  class="button is-success is-rounded" @click="onSubmit">Add</button>
+        </footer>
+    </div>       
+</div>
+
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    props: ['organisations','roles','currentRole'],
+  data() {
+    return {
+    Data: {"name": "", "firstname":"", "email":"", "role":0, "organisation": 0},
+    Organisations: this.organisations,
+    Roles: this.roles
+    };
+  },
+  methods: {
+    onSubmit() {
+        const path = '/api/register/'
+        const dataJson = JSON.stringify(this.Data);
+        axios.post(path, dataJson, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          withCredentials: true
+        })
+        .then((response) => {
+            this.$emit("item-added", response.data.dataobj);            
+            this.$emit("success", response.data.message);    
+            this.$emit("close-pop-up");           
+       
+        }).catch((err)=>{    
+            this.$emit('error',err.response.data.message);                          
+            this.$emit("close-pop-up");           
+        })
+    },
+    cancel(){
+        this.$emit("close-pop-up");
+    }
+  }   
+};
+
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+.delete-model{
+    display:flex;
+    .add-item{
+        display:flex;
+        flex-direction:column;
+        width:100%;
+        margin:0% 0 2%;
+        padding:0px 8%;
+        label{
+            font-family:'Roboto';
+            font-weight:bold;
+        }
+        input,select{
+            height:40px;
+            font-size:1.2em;
+            margin-bottom:5%;
+
+        }
+    }
+  }
+   
+
+</style>
