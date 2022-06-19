87a80f6f852b93f30f8317e738f5a4db1c957e3c
merlin-dot <merlinreiter@live.com>
2022-06-19 21:55:16 +0000

    made code cleaner

--- prototype/backend/api/register/register.py
+++ prototype/backend/api/register/register.py
@@ -43,75 +43,8 @@ def sendRegistration(name, password, mailAddress):
                  "Dein Password ist: {password} ")
 
     # The HTML body of the email.
-    BODY_HTML = """
-<html lang="en-US">
+    BODY_HTML = render_template('set-password.html', username = name, password=password)
 
-<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
-    <!--100% body table-->
-    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
-        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
-        <tr>
-            <td>
-                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
-                    align="center" cellpadding="0" cellspacing="0">
-                    <tr>
-                        <td style="height:80px;">&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td style="text-align:center;">
-                            <a href="https://wirfuerschule.lio.codes/">
-                            <img width="200" src="https://images-ext-2.discordapp.net/external/g0fCuKzVPH_ke9aSGSsSWLl0MI3Qy04Xfq6e0yoqsGI/https/wirfuerschule.lio.codes/img/wirfuerschuleLogo.0f60481e.png" title="logo" alt="logo">
-                        </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="height:20px;">&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <td style="padding:0 35px;">
-                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;"><p>Hallo {{username}},</p>
-                                        </h1>
-                                        <span
-                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
-                                        <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
-                                            <p>Dein Passwort ist: {{password}}</p>
-
-                                        </p>
-
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                            </table>
-                        </td>
-                    <tr>
-                        <td style="height:20px;">&nbsp;</td>
-                    </tr>
-                    <tr>
-                    <tr>
-                        <td style="height:80px;">&nbsp;</td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-    <!--/100% body table-->
-</body>
-
-</html>"""
-
-    BODY_TEXT = BODY_TEXT.replace("{name}", name)
-    BODY_TEXT = BODY_TEXT.replace("{password}", password)
-    BODY_HTML = BODY_HTML.replace("{{username}}", name)
-    BODY_HTML = BODY_HTML.replace("{{password}}", password)
     msg = MIMEMultipart('alternative')
     msg['Subject'] = SUBJECT
     msg['From'] = formataddr((SENDERNAME, SENDER))
