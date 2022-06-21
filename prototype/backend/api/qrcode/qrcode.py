ab0647ae68db606d0b1ead5fc8dbe7f48cbaecf8
gwhinbleid <goku_juniorssj@web.de>
2022-06-22 00:38:12 +0200

    fix code

--- prototype/backend/api/qrcode/qrcode.py
+++ prototype/backend/api/qrcode/qrcode.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, send_file
 from app import db
 from models.hackathon import Hackathon as Hackathonmodel
 from models.token import Token as Tokenmodel
 from utils.user_roles import auth_required, Config
-from utils.pdf import PDF
+from datetime import datetime
+from fpdf import FPDF
 import qrcode
-import base64
-import io
+import time
+import os
+
 
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
@@ -20,6 +22,16 @@ def create_qrcode(hackathonid):
     result = db.session.query(Tokenmodel.tokenid, Hackathonmodel.slug).join(Hackathonmodel).filter(Hackathonmodel.hackathonid==hackathonid).all()
     
     j = 0
+    suffix_png = ".png"
+    suffix_pdf = ".pdf"
+    p = FPDF('P', 'mm', 'A4')
+    p.set_font('Arial', 'B', 16)
+
+    current_time = time.time()
+    date_time = datetime.fromtimestamp(current_time)
+    tstamp = date_time.strftime("%Y%m%d%H%M%S")
+
+
     for i in result:
         j = j+1
         tokenid = i[0]
@@ -30,14 +42,15 @@ def create_qrcode(hackathonid):
         qr.add_data(url)
         qr.make(fit = True)
         img = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask())
-        buffered = io.BytesIO()
-        img.save(buffered, format="PNG")
-        #img_str = base64.b64encode(buffered.getvalue()).decode("utf-8").encode()
-        fname = "wirfuerschule{0}.png".format(j)
+        fname = "wirfuerschule{0}{1}".format(j, suffix_png)
         img.save(fname)
-        #p = PDF()
-        #p.add_page()
-        #pdf.add_image(img)
-        #p.sample_pdf(img_str, fname, p)
-    return "Nix"
+
+        p.add_page()
+        p.cell(100, 40, 'QRCode ID: {0}'.format(tokenid))
+        p.image(fname, x=70, y=100, w=80, h=80)
+        os.remove(fname)
+
+    pdffile = "wirfuerschule{0}{1}".format(tstamp, suffix_pdf)
+    p.output(pdffile) 
+    return send_file(pdffile, mimetype = 'pdf', attachment_filename = pdffile, as_attachment = True)
 
